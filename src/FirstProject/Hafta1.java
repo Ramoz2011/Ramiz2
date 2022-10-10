b9541914d8d4d177275f94c93fc0fdc492923f4f
ramiz mert <ramizmert@ramizs-MBP.lan>
2022-10-09 20:50:34 -0400

    Initial commit

---
+++ src/FirstProject/Hafta1.java
@@ -0,0 +1,18 @@
+package FirstProject;
+
+public class Hafta1 {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println(3*2);
+        moveForward();
+    }
+
+
+    public static  void moveForward(){
+
+
+
+    }
+
+
+}
---
+++ src/FirstProject/Hafta4.java
@@ -0,0 +1,7 @@
+package FirstProject;
+
+public class Hafta4 {
+    public static void main(String[] args) {
+
+    }
+}
